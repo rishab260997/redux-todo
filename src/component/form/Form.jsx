be2444956045cad1c19ba84b72336678d84ae12e
imsandip10 <sandybro9919@gmail.com>
2020-06-26 20:05:53 +0530

    hooks

--- src/component/form/Form.jsx
+++ src/component/form/Form.jsx
@@ -1,69 +1,74 @@
-import React, {useState} from 'react';
-import TodoInput from '../inputBox/InpputBox';
-import SubmitButton from '../submitButton/SubmitButton';
+import React, { useState } from "react";
+import TodoInput from "../inputBox/InpputBox";
+import SubmitButton from "../submitButton/SubmitButton";
 // import { useState } from 'react';
-import { CardContent, Card, } from "@material-ui/core";
+import { CardContent, Card } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import DatePicker from "react-date-picker";
 
+function TodoForm() {
+  // const [title, setTitle] = useState("");
+  const [todoItem, setTodoItem] = useState([]);
+  // const [date, setDate] = useState(new Date());
+  const [formState, changeFormStates] = useState({title: "", date: new Date()})
 
 
-function TodoForm () {
-    const [title, setTitle] = useState("");
-    const [todoItem, setTodoItem] = useState([]); 
-    const [date, setDate] = useState(new Date());
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!formState.title == "") {
+      let todo = {
+        id: Math.random(),
+        todo: formState.title,
+        completed: false,
+        // dates: date.toLocaleDateString(),
+      };
+      setTodoItem([...todoItem, todo]);
+    }
+    console.log(todoItem,"rrrrrrrrrrrrrrrrrrrr"); 
+    // setTitle("");
+  };
 
-    console.log(todoItem,"dddddddddddddddd");
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      if (!title == "") {
-        let todo = {
-          id: Math.random(),
-          todo: title,
-          completed: false,
-          dates: date.toLocaleDateString(),
-        };
-        setTodoItem([...todoItem, todo]);
-      }
-      setTitle(""); 
-    };
+  const handleChange = (event) => {
+    // setTitle(event.target.value);
+    changeFormStates({...formState, title :  event.target.value})
+  };
 
-    const handleChange = (event) => {
-        setTitle(event.target.value);        
-    };
-    
-    const useStyles = makeStyles((theme) => ({
-        root: {
-          "& > *": {
-            margin: theme.spacing(1),
-          },
-        },
-        title: {
-          flexGrow: 1,
-        },
-        cards: {
-          minHeight: "50vh",
-        },
-      }));
-      const classes = useStyles();
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+      },
+    },
+    title: {
+      flexGrow: 1,
+    },
+    cards: {
+      minHeight: "50vh",
+    },
+  }));
+  const classes = useStyles();
 
-    return(
-        <div>
-        <Card className={classes.cards}>
-          <CardContent>
-            <form
-              onSubmit={handleSubmit}
-              className={classes.root}
-              noValidate
-              autoComplete="off"
-            >
-            <TodoInput handleChange={(event) => {handleChange(event)}}/>
-            <DatePicker  value={date} onChange={(date) => setDate(date)}/>
-            <SubmitButton  />
-            </form>
-          </CardContent> 
-        </Card>
-        </div>
-    )
+  return (
+    <div>
+      <Card className={classes.cards}>
+        <CardContent>
+          <form
+            onSubmit={handleSubmit}
+            className={classes.root}
+            noValidate
+            autoComplete="off"
+          >
+            <TodoInput
+              handleChange={(event) => {
+                handleChange(event);
+              }}
+            />
+            <DatePicker value={formState.date} onChange={ () =>  changeFormStates(formState.date)} />
+            <SubmitButton />
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 export default TodoForm;
--- src/component/submitButton/SubmitButton.jsx
+++ src/component/submitButton/SubmitButton.jsx
@@ -1,29 +1,26 @@
-import React from 'react';
-import { Button} from "@material-ui/core";
+import React from "react";
+import { Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
+function SubmitButton(props) {
+  const useStyles = makeStyles((theme) => ({
+    boxs: {
+      display: "inline-block",
+    },
+  }));
+  const classes = useStyles();
 
-
-function SubmitButton (props) {
-
-    const useStyles = makeStyles((theme) => ({
-        boxs: {
-           display: "inline-block"
-        },
-      }));
-      const classes = useStyles();
-
-    return(
-        <div className={classes.boxs}>
-            <Button
-                variant="contained"
-                color="secondary"
-                type="submit"
-                value="Submit"
-                // onSubmit ={(e) =>{props.handleSubmit(e)}}
-              >Submit
-            </Button>
-        </div>
-    )
+  return (
+    <div className={classes.boxs}>
+      <Button
+        variant="contained"
+        color="secondary"
+        type="submit"
+        value="Submit"
+      >
+        Submit
+      </Button>
+    </div>
+  );
 }
 export default SubmitButton;
