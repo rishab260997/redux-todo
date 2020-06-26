d2bdbe9c4d125181d4c7833832d14b2e14badadd
imsandip10 <sandybro9919@gmail.com>
2020-06-26 14:21:27 +0530

    folder str

---
+++ src/pages/Page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../component/navbar/Navbar';
+
+function Page (){
+    return(
+      <div>
+       <Navbar />
+      </div>
+    )
+}
+
+export default Page;
